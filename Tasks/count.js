2a5ff44271af9d21715f37ba73adc6bd458a9609
ermishins <ermishins@gmail.com>
2024-01-13 14:31:39 +0200

    Replaced forEach to for...of, count is refactored

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,15 +1,19 @@
 // Sum all number values in dict
 
+// count.js is refactored, Test count.js: Passed: 6 of 6
+
 'use strict';
 
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
 
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+    if (typeof value === 'number') {
+      sum += value;
+    }
+  }
 
   return sum;
 };
