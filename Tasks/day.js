d8b58d11023c9e18cc82889b85565d113c21dda2
ermishins <ermishins@gmail.com>
2024-01-13 14:33:13 +0200

    Add use strict, indentations and spacing

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,18 @@
 // Get day number
 
+'use strict';
+
 let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const _parse_day_ = (s) => {
   let i;
+
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
   }
+
   return -1;
 };
 
