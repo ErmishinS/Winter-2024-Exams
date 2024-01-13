1be072340f9e2ff66126564c8026293a7378a68d
ermishins <ermishins@gmail.com>
2024-01-13 14:58:51 +0200

    Fixed naming

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -2,13 +2,13 @@
 
 'use strict';
 
-const inc_numbers = (format_complete, ...rest_variables) => {
-  for (const delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+const incrementNumbers = (formatComplete, ...restVariables) => {
+  for (const key in formatComplete) {
+    if ((typeof formatComplete[key]).charAt(0).toUpperCase() === 'N') {
+      formatComplete[key] = formatComplete[key] + 1;
     }
   }
-  return format_complete;
+  return formatComplete;
 };
 
-module.exports = inc_numbers;
+module.exports = incrementNumbers;
